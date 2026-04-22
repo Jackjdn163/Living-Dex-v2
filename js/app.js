d9fe11e08520f638ba75fcfdd62726ea635144d1
Jackjdn163 <159971083+Jackjdn163@users.noreply.github.com>
2026-04-21 20:48:15 -0400

    Update app.js

--- js/app.js
+++ js/app.js
@@ -137,8 +137,6 @@ async function showDetail(p) {
   document.getElementById("modal-name").textContent = `#${p.id} ${p.name}`;
   document.getElementById("modal-sprite").src = getSprite(p);
   // Force modal to be scrollable and fully visible
-  modal.style.maxHeight = "95vh";
-  modal.style.overflowY = "auto";
   const typesDiv = document.getElementById("modal-types");
   const evoDiv = document.getElementById("modal-evo");
   const gamesDiv = document.getElementById("modal-games");
@@ -168,6 +166,9 @@ async function showDetail(p) {
   } else {
     evoDiv.innerHTML = "No evolution data";
   }
+  modal.style.maxHeight = "95vh";
+  modal.style.overflowY = "auto";
+  
 
   // ===================== SWITCH GAMES (Base + ALL DLC) =====================
   const switchGameMap = {
